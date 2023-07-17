366447806cd9ee1442da6e433967fdfc6934645d
PiotrKukula0 <piotr.kukula@green-code.studio>
2023-07-17 16:21:27 +0200

    Zadanie6

--- Zadanie/src/Controller/ExchangeController.php
+++ Zadanie/src/Controller/ExchangeController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ExchangeController
@@ -52,5 +53,68 @@ class ExchangeController
 
        
         return new JsonResponse(['first' => $first, 'second' => $second]);
+    }
+
+    /**
+     * @Route("/history", methods={"GET", "POST"})
+     */
+    public function getHistory(Request $request, PaginatorInterface $paginator): JsonResponse
+    {
+        if ($request->isMethod('POST')) {
+            $data = json_decode($request->getContent(), true);
+
+            $first = $data['first'];
+            $second = $data['second'];
+
+            $temp = $first;
+            $first = $second;
+            $second = $temp;
+
+            $now = new \DateTime();
+
+            $history = new History();
+            $history->setFirstIn($first);
+            $history->setSecondIn($second);
+            $history->setFirstOut($second);
+            $history->setSecondOut($first);
+            $history->setCreatedAt($now);
+            $history->setUpdatedAt($now);
+
+            $this->entityManager->persist($history);
+            $this->entityManager->flush();
+
+            return new JsonResponse(['message' => 'Data saved successfully']);
+        }
+
+        $page = $request->query->getInt('page', 1);
+        $limit = $request->query->getInt('limit', 10);
+        $sortColumn = $request->query->get('sort_column', 'id');
+        $sortOrder = $request->query->get('sort_order', 'asc');
+
+        $historyRepository = $this->entityManager->getRepository(History::class);
+        $queryBuilder = $historyRepository->createQueryBuilder('h')
+            ->orderBy('h.' . $sortColumn, $sortOrder);
+
+        $pagination = $paginator->paginate($queryBuilder, $page, $limit);
+
+        $data = [];
+        foreach ($pagination as $record) {
+            $data[] = [
+                'id' => $record->getId(),
+                'first_in' => $record->getFirstIn(),
+                'second_in' => $record->getSecondIn(),
+                'first_out' => $record->getFirstOut(),
+                'second_out' => $record->getSecondOut(),
+                'created_at' => $record->getCreatedAt()->format('Y-m-d H:i:s'),
+                'updated_at' => $record->getUpdatedAt()->format('Y-m-d H:i:s'),
+            ];
+        }
+
+        return new JsonResponse([
+            'data' => $data,
+            'total_items' => $pagination->getTotalItemCount(),
+            'current_page' => $pagination->getCurrentPageNumber(),
+            'items_per_page' => $pagination->getItemNumberPerPage(),
+        ]);
     }
 }
